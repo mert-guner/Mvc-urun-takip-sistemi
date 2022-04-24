f4c26c816481af31734d9b9a2098eebdc1935f6d
Mert Guner <mertcannguner@gmail.com>
2022-04-24 16:59:59 +0300

    mvc

---
+++ proje/proje/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using proje.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proje.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+
+        ProjeEntities db = new ProjeEntities();
+
+        // GET: Home
+        //[Authorize]
+        public ActionResult Index()
+        {
+            var model = db.Urun_tbl.ToList();
+            return View(model);
+        }
+
+        //[Authorize]
+        [HttpGet]
+        public ActionResult Yeni()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Yeni(Urun_tbl p1)
+        {
+            if (p1.UrunID == 0)
+            {
+                db.Urun_tbl.Add(p1);
+            }
+            else 
+            {
+                var guncellenecekdepartman = db.Urun_tbl.Find(p1.UrunID);
+
+                if (guncellenecekdepartman == null)
+                {
+                    return HttpNotFound();
+                }
+
+                guncellenecekdepartman.urun_adi = p1.urun_adi;
+                guncellenecekdepartman.urun_aciklama = p1.urun_aciklama;
+                guncellenecekdepartman.urun_fiyat = p1.urun_fiyat;
+                guncellenecekdepartman.stok = p1.stok;
+            }
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var silinicekdepartman = db.Urun_tbl.Find(id);
+
+            if (silinicekdepartman == null)
+            {
+                return HttpNotFound();
+            }
+
+            db.Urun_tbl.Remove(silinicekdepartman);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Guncelle(int id)
+        {
+            var model = db.Urun_tbl.Find(id);
+
+            if (model == null)
+            {
+                return HttpNotFound();
+
+            }
+
+            return View("Yeni", model);
+        }
+
+
+
+    }
+}
---
+++ proje/proje/Controllers/LoginController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using proje.Models;
+using System.Web.Security;
+
+namespace proje.Controllers
+{
+    public class LoginController : Controller
+    {
+        ProjeEntities db = new ProjeEntities();
+
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {          
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(login_tbl login)
+        {
+            var bilgiler = db.login_tbl.FirstOrDefault(x => x.kullanici_adi == login.kullanici_adi && x.sifre == login.sifre);
+
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.kullanici_adi, false);
+                Session["kullanici_adi"] = bilgiler.kullanici_adi;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                
+                return View();
+            }
+            
+        }
+
+        public ActionResult Uye()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Uye(login_tbl kayit)
+        {
+            db.login_tbl.Add(kayit);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Login");
+        }
+
+
+        public ActionResult Cikis()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
+        }
+
+    }
+}
